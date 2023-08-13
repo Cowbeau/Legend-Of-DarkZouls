df37e9a48d009c67641918d185b27bb5a1c49106
beaum <beaumccoy95@gmail.com>
2023-08-13 15:42:14 -0500

    fix: :art: Fix images
    Fixed missing self.image.rect code needed

--- Legend Of DarkZouls/code/magic.py
+++ Legend Of DarkZouls/code/magic.py
@@ -1,5 +1,5 @@
 import pygame
-from settings import *
+from settings import *  # noqa: F403
 from random import randint
 
 class MagicPlayer:
@@ -13,7 +13,7 @@ class MagicPlayer:
             if player.health >= player.stats['health']:
                 player.health = player.stats['health']
             self.animation_player.create_particles('aura',player.rect.center.groups)
-            self.animation_player.create_particles('heal',player.rect.center + pygame.math,Vector2(0,-60).groups)
+            self.animation_player.create_particles('heal',player.rect.center + pygame.math,Vector2(0,-60).groups)  # noqa: E501, F405
 
     def flame(self,player,cost,groups):
         if player.energy >= cost:
