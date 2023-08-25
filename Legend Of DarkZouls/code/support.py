f048fa66f49a6b525490f8056d8a754c26dc3f8b
beaum <beaumccoy95@gmail.com>
2023-08-24 21:45:27 -0500

    fix: :art: Fixing bugs
    Fixing bugs with bars not showing, etc

--- Legend Of DarkZouls/code/support.py
+++ Legend Of DarkZouls/code/support.py
@@ -18,6 +18,5 @@ def import_folder(path):
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
-
     return surface_list
 
